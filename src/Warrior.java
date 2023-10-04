b50fc798ba12682af3c4c1e146c48337fc6c0a38
ZainabHY <zhalyousif@gmail.com>
2023-10-04 13:52:26 +0300

    Modifying Warrior Class and Re-run the code

--- src/Warrior.java
+++ src/Warrior.java
@@ -1,80 +1,109 @@
 import java.util.Random;
-
-
 public class Warrior extends Character {
     private int stamina;
     private int strength;
-    Random random = new Random();
 
-    public Warrior(String name, int hp, int stamina, int strength) {
-        super(name, hp);
-        this.stamina = stamina;
-        this.strength = strength;
+    public static int generateRandomNumber(int min, int max) {
+        // Create an instance of the Random class
+        Random random = new Random();
 
-//        this.stamina = generateStamina(); // Generate random STAMINA in this range (10-50)
-//        this.strength = generateStrength(); // Generate random STRENGTH in this range (1-10)
+        // Generate a random number between min and max (inclusive)
+        return random.nextInt((max - min) + 1) + min;
     }
 
-    // Getters and Setters
+    //
+    public Warrior(String name, int hp, int stamina,int strength) {
+        super(name, hp);
+        this.strength = strength;
+        this.stamina= stamina;
+    }
 
+
+
+
+    //get methods for stamina. however, the stamina value already sat randomly on parent class
     public int getStamina() {
         return stamina;
     }
 
-    // Set for STAMINA by Generating random number between 10-50
-//    private int generateStamina()
-//    {
-//        return random.nextInt(10,51);
-//    }
-
+    //get methods for strength. however, the stamina value already sat randomly on parent class
     public int getStrength() {
         return strength;
     }
 
-    // Set for STRENGTH by Generating random number between 10-50
-//    private int generateStrength()
-//    {
-//        return random.nextInt(1, 11);
-//    }
 
-    // Minimum HP for a Warrior
+
+
+    public static String generateRandomHitType() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(2); // Generates either 0 or 1
+
+        // Use the random number to select the hit type
+        if (randomNumber == 0) {
+            return "weak";
+        } else {
+            return "heavy";
+        }
+
+    }
+    @Override
+    public void attack(Character c) {
+        String hitType = generateRandomHitType();
+        if (!isAlive()) {
+            System.out.println(getName() + " is not alive and cannot attack.");
+            return;
+        }
+
+        if (stamina >= 5) {
+            if (hitType == "heavy")
+            {
+                int damage = strength;
+                c.setHp(c.getHp() - damage);
+                stamina = stamina -5;
+                System.out.println(getName() +" performs a Heavy attack on " + c.getName() + " for " + damage + " damage." );
+
+            }
+
+            //hitType is equal to weak
+            else {
+                int damage = strength / 2; // Weak attack deals half of the strength damage
+                c.setHp(c.getHp() - damage);
+                stamina += 1; // Recover 1 stamina point with a Weak attack
+                System.out.println(getName() + " performs a Weak attack on " + c.getName() + " for " + damage + " damage.");
+            }
+
+        }
+        //when warrior have less stamina
+        else if (stamina>=0){
+            if (hitType == "heavy")//perform a weak attack
+            {
+                int damage = strength/2;
+                c.setHp(c.getHp() - damage);
+                stamina -= 5;
+                System.out.println(getName() + " performs a Weak attack on " + c.getName() + " for " + damage + " damage since the stamina is not enough for a heavy attack");
+
+
+            }
+            //hitType is equal to weak
+            else
+            {
+                int damage = 0;
+                c.setHp(c.getHp()-damage);
+                stamina += 2;
+                System.out.println(getName() + " performs a Weak attack on " + c.getName() + " for " + damage + " damage");
+
+            }
+
+        }
+    }
+
     @Override
     protected int getMinHP() {
         return 100;
     }
 
-    // Maximum HP for a Warrior
     @Override
     protected int getMaxHP() {
         return 200;
-    }
-
-
-    // Overloaded attack() method for Warriors
-    @Override
-    public void attack(Character character) {
-        int attackType = random.nextInt(2); // 0 for Heavy attack, 1 for Weak attack
-
-        if (attackType == 0) {
-            if (stamina >= 5) {
-                int heavyAttackDamage = strength; // Damage based on the warrior's strength
-                character.setHp(character.getHp() - heavyAttackDamage); // Reduce target's health
-                stamina -= 5; // Consume stamina for the heavy attack
-                System.out.println(getName() + " performs a Heavy attack on " + character.getName() + " causing " + heavyAttackDamage + " damage");
-            } else {
-                System.out.println(getName() + " doesn't have enough stamina to perform a Heavy attack");
-                attack(character); // Retry with a Weak attack
-            }
-        } else {
-            if (stamina >= 1) {
-                int weakAttackDamage = strength / 2; // Damage is half of the warrior's strength
-                character.setHp(character.getHp() - weakAttackDamage); // Reduce target's health
-                stamina += 1; // Recover stamina for the weak attack
-                System.out.println(getName() + " performs a Weak attack on " + character.getName() + " causing " + weakAttackDamage + " damage");
-            } else {
-                System.out.println(getName() + " doesn't have enough stamina to perform a Weak attack");
-                stamina += 2; // Recover stamina by 2
-            }
-        }
     }
 }
